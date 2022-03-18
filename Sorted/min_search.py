9a6521d592fa76b38c4c0c3a1ed010f5c72dbba1
Фуников Андрей Дмитриевич <funikov@bsu.edu.ru>
2022-03-18 18:53:52 +0300

    Поиск минимального

--- Sorted/min_search.py
+++ Sorted/min_search.py
@@ -1,14 +1,29 @@
 # Поиск минимального
 
+# Наша строка
+string = '123 22'
 
-def search_min(array):
-    min = len(array[0])
-    for i in range(0,len(array)):
-        print('Минимум на текущем шаге', min)
-        if len(array[i]) < min:
-            min = len(array[i])
 
-    return min
-array = ['2','5','10','','1','16','7']
-sam_korot = search_min(array)
-print(sam_korot)
+def search_min_in_string(string):
+    i = 0
+    # sam_korot = 0
+    while i <= len(string)-1:
+        word = ''
+        while string[i] != ' ':
+            word = word + string[i]
+            i = i + 1
+    print(word)
+
+
+
+
+#
+# def search_min(array):
+#     min = len(array[0])
+#     for i in range(0,len(array)):
+#         print('Минимум на текущем шаге', min)
+#         if len(array[i]) < min:
+#             min = len(array[i])
+#     return min
+
+(search_min_in_string(string))
